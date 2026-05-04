9845862dadb14ce5c4a77d6857110f5e6b77491d
Thao-Uyen-1703 <uyenpu1703@gmail.com>
2026-05-04 16:06:28 +0700

    feat:get product list and with id

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const productRoute = require('./routes/productRoute');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/product', productRoute);
 
 app.listen(port, () => {
     console.log(`Running at port:${port}`);
