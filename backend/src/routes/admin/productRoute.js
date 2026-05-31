78ad59d01981b972c8f56cfda251872fc09594f2
Thao-Uyen-1703 <uyenpu1703@gmail.com>
2026-05-31 12:12:17 +0700

    feat:authorize for role based

--- backend/src/routes/admin/productRoute.js
+++ backend/src/routes/admin/productRoute.js
@@ -2,16 +2,19 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../../controllers/admin/productController');
 const uploadMiddleware = require('../../middleware/uploadMiddleware');
+const authMiddleware = require('../../middleware/authMiddleware');
+const roleMiddlware = require('../../middleware/roleMiddleware');
 
-// router.use();
+router.use(authMiddleware);
+router.use(roleMiddlware('Staff', 'Manager', 'Admin'));
 
 router.get('/', productController.getAll);
 router.get('/:id', productController.getById);
 
 router.post('/', uploadMiddleware.single('image'), productController.create);
 router.put('/:id', uploadMiddleware.single('image'), productController.update);
-router.patch('/:id/toggle-status', productController.toggleStatus);
 
-router.delete('/:id', productController.delete);
+router.patch('/:id/toggle-status', roleMiddlware('Manager', 'Admin'), productController.toggleStatus);
+router.delete('/:id', roleMiddlware('Manager', 'Admin') ,productController.delete);
 
 module.exports = router;
